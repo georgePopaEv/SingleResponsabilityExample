93b5b7170bae54b13b6c262bd308177461385209
GeorgePopaA <poplike14@yahoo.com>
2023-01-05 13:33:33 +0200

    I have updated the code for last 2 created classess not beeing errors. I didn't develope too much code but I updated with a default return code for both of them.

--- src/Finance.java
+++ src/Finance.java
@@ -1,5 +1,6 @@
 public class Finance {
     public  double calcIncomeTaxForCurrentYear(Employee emp) { // it is finance responsability
         // income tax logic using the emplyee passed in
+        return 0.0;
     }
 }
